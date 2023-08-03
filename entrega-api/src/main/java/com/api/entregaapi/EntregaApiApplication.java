93d6ecdb36bf9101d24ba255f55df4c89bfa0718
juliusarc <julius.cgm@gmail.com>
2023-08-02 23:48:56 -0300

    First commit: adicionei tabela cliente e ligação com o banco de dados mysql (e alterei uma tabela)

---
+++ entrega-api/src/main/java/com/api/entregaapi/EntregaApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.entregaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregaApiApplication.class, args);
+	}
+
+}
